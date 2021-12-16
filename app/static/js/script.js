07e422a9d593dc58bb44e3af4231321a2e960ff4
Vadim <71180434+ellastyko@users.noreply.github.com>
2021-12-16 17:12:52 +0200

    .gitignore and config fix

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -31,7 +31,6 @@ async function post(url, data) {
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
               'Content-Type': 'application/json'
-              // 'Content-Type': 'application/x-www-form-urlencoded',
             },
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
